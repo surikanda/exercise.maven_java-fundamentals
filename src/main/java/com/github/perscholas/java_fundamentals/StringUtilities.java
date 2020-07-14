77971d69cfde554c3920c7a59de167207dc480e2
suresh kandagatla <surikandagatla@gmail.com>
2020-07-14 15:19:08 -0400

    Fixed getMiddle character function. Finally finished

--- src/main/java/com/github/perscholas/java_fundamentals/StringUtilities.java
+++ src/main/java/com/github/perscholas/java_fundamentals/StringUtilities.java
@@ -60,7 +60,9 @@ public class StringUtilities {
      * @return the middle character of `inputValue`
      */
     public static Character getMiddleCharacter(String inputValue){
-        return inputValue.charAt(inputValue.length()/2);
+        if (inputValue.length()%2 == 0){
+            return inputValue.charAt((inputValue.length()/2)-1);
+        } else return inputValue.charAt(inputValue.length()/2);
     }
 
     /**
